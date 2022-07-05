c3586e0758b6730b2705c586ef687c0088a9f5d1
ewen <ewen.grosjean@gmail.com>
2022-07-05 10:59:05 +0200

    1.1

--- src/badigeon2/utils.clj
+++ src/badigeon2/utils.clj
@@ -2,7 +2,7 @@
   (:require [clojure.set :as set]
             [clojure.spec.alpha :as s]))
 
-(def ^:const version "1.0")
+(def ^:const version "1.1")
 
 (defn assert-required
   "Check that each key in required coll is a key in params and throw if
